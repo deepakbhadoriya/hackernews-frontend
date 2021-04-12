1766d665b3da0b4ca8f1347ac94ffa830ad74e59
deepakbhadoriya <deepakkbhadoriya@gmail.com>
2021-04-13 00:30:26 +0530

    complete frontend

---
+++ src/components/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
+
+import Header from './Header';
+import Login from './Login';
+import Search from './Search';
+import LinkList from './LinkList';
+import CreateLink from './CreateLink';
+
+const App = () => {
+  return (
+    <div className="center w85">
+      <Header />
+      <div className="ph3 pv1 background-gray">
+        <Switch>
+          <Route exact path="/" render={() => <Redirect to="/new/1" />} />
+          <Route exact path="/create" component={CreateLink} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/search" component={Search} />
+          <Route exact path="/top" component={LinkList} />
+          <Route exact path="/new/:page" component={LinkList} />
+        </Switch>
+      </div>
+    </div>
+  );
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,60 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { setContext } from '@apollo/client/link/context';
+import { BrowserRouter } from 'react-router-dom';
+import { split } from '@apollo/client';
+import { WebSocketLink } from '@apollo/client/link/ws';
+import { getMainDefinition } from '@apollo/client/utilities';
+import { ApolloProvider, ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client';
 
+import './styles/index.css';
+import App from './components/App';
+import reportWebVitals from './reportWebVitals';
+import { AUTH_TOKEN } from './constants';
+
+const httpLink = createHttpLink({ uri: 'http://localhost:4000' });
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem(AUTH_TOKEN);
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const wsLink = new WebSocketLink({
+  uri: `ws://localhost:4000/graphql`,
+  options: {
+    reconnect: true,
+    connectionParams: {
+      authToken: localStorage.getItem(AUTH_TOKEN),
+    },
+  },
+});
+
+const link = split(
+  ({ query }) => {
+    const { kind, operation } = getMainDefinition(query);
+    return kind === 'OperationDefinition' && operation === 'subscription';
+  },
+  wsLink,
+  authLink.concat(httpLink)
+);
+
+const client = new ApolloClient({
+  link,
+  cache: new InMemoryCache(),
+});
+
+// 4
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
